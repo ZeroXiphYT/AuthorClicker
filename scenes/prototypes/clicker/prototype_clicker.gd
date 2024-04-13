3aa995b6abc52c538ece5779e776037092036f0e
ZeroXiphYT <vachanbharucha11@gmail.com>
2024-04-13 16:28:37 -0700

    ✏️ Added prototype

---
+++ scenes/prototypes/clicker/prototype_clicker.gd
@@ -0,0 +1,24 @@
+class_name PrototypeClicker
+extends Control
+
+@export var label : Label
+
+
+var booksWritten : int = 0
+var writingSpeed : int = 1
+
+
+func write_book() -> void:
+	booksWritten += writingSpeed
+	update_label_text()
+
+
+func update_label_text() -> void:
+	label.text = "Books Written : %s" %booksWritten
+
+
+func _ready() -> void:
+	update_label_text()
+
+func _on_button_pressed() -> void:
+	write_book()
